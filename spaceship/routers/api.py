7706c39bfb77e60357e37ebe186ae345547a0f00
DmytroHalai <dimagalai04@gmail.com>
2025-05-20 19:20:28 +0300

    Add matrix creating and multiplication
    Docker base image is python:3.11-alpine

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,3 +1,4 @@
+import numpy as np
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -6,3 +7,16 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+
+@router.get("/multiply-matrices")
+def multiply_matrices():
+    a = np.random.randint(0, 10, size=(10, 10))
+    b = np.random.randint(0, 10, size=(10, 10))
+    result = np.matmul(a, b)
+
+    return {
+        "matrix_a": a.tolist(),
+        "matrix_b": b.tolist(),
+        "product": result.tolist()
+    }
